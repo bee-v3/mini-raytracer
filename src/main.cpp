b9adddffd432a0df094be4ceb8039d5586b44453
bvelazq <88893930+bvelazq@users.noreply.github.com>
2022-12-28 01:23:41 -0500

    Add ray class

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #define STBI_ONLY_PNG
+#include <vector>
+#include <iostream>
+#include "vec3.h"
+#include "ray.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image_write.h>
-#include <vector>
-#include "vec3.h"
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
 
 int main()
 {/*
@@ -13,18 +21,40 @@ int main()
     stbi_write_png("image2.png", width, height, channels, data, width * channels);
     stbi_image_free(data);
 */
-    int width = 512, height = 512, channels = 3;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int img_width = 400;
+    const int img_height = static_cast<int>(img_width / aspect_ratio);
+    const int channels = 3;
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
     const char* filename = "png_output.png";
-    std::vector<char> imgbuffer(width * height * channels);
-        for (int j = height - 1; j >= 0; --j) {
-            for (int i = 0; i < width; i++) {
-                color pixel(double(i) / (width - 1), (height - 1 - double(j)) / (height - 1), 0.25);
-                imgbuffer[(j * height + i) * channels + 0] = pixel.x() * 255;
-                imgbuffer[(j * height + i) * channels + 1] = pixel.y() * 255;
-                imgbuffer[(j * height + i) * channels + 2] = pixel.z() * 255;
+
+    std::vector<char> imgbuffer(img_width * img_height * channels);
+        for (int j = img_height - 1; j >= 0; --j) {
+
+            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            
+            for (int i = 0; i < img_width; ++i) {
+                auto u = double(i) / (img_width - 1);
+                auto v = double(j) / (img_height - 1);
+                ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+                color pixel = ray_color(r);
+                imgbuffer[(j * img_height + i) * channels + 0] = pixel.x() * 255;
+                imgbuffer[(j * img_height + i) * channels + 1] = pixel.y() * 255;
+                imgbuffer[(j * img_height + i) * channels + 2] = pixel.z() * 255;
                 
             }
         }
-    stbi_write_png(filename, width, height, channels, imgbuffer.data(), 0);
+    stbi_write_png(filename, img_width, img_height, channels, imgbuffer.data(), 0);
+    std::cerr << std::endl << "Done" << std::endl;
     return 0;
 }
