55ec1284f4a2098955f6a8c1cf11b7177256b15c
bee-v3 <19674875+bee-v3@users.noreply.github.com>
2023-01-29 16:39:26 -0500

    Fix math in filling pixel data

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define STBI_ONLY_PNG
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include "vec3.h"
 #include "ray.h"
 #define STB_IMAGE_IMPLEMENTATION
@@ -15,14 +16,9 @@ color ray_color(const ray& r) {
 }
 
 int main()
-{/*
-    int width, height, channels;
-    unsigned char* data = stbi_load("image.png", &width, &height, &channels,4);
-    stbi_write_png("image2.png", width, height, channels, data, width * channels);
-    stbi_image_free(data);
-*/
+{
     const auto aspect_ratio = 16.0 / 9.0;
-    const int img_width = 400;
+    const int img_width = 600;
     const int img_height = static_cast<int>(img_width / aspect_ratio);
     const int channels = 3;
 
@@ -39,22 +35,26 @@ int main()
     const char* filename = "png_output.png";
 
     std::vector<char> imgbuffer(img_width * img_height * channels);
-        for (int j = img_height - 1; j >= 0; --j) {
+    
+    // Counter for the image buffer
+    int index = 0;
+    // Loop through each pixel in the image
+    for (int j = img_height - 1; j >= 0; --j) {
+        
+        for (int i = 0; i < img_width; ++i) {
+            auto u = double(i) / (img_width - 1);
+            auto v = double(j) / (img_height - 1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel = ray_color(r);
 
-            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            imgbuffer[index++] = pixel.x() * 255;
+            imgbuffer[index++] = pixel.y() * 255;
+            imgbuffer[index++] = pixel.z() * 255;
             
-            for (int i = 0; i < img_width; ++i) {
-                auto u = double(i) / (img_width - 1);
-                auto v = double(j) / (img_height - 1);
-                ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-                color pixel = ray_color(r);
-                imgbuffer[(j * img_height + i) * channels + 0] = pixel.x() * 255;
-                imgbuffer[(j * img_height + i) * channels + 1] = pixel.y() * 255;
-                imgbuffer[(j * img_height + i) * channels + 2] = pixel.z() * 255;
-                
-            }
         }
+    }
     stbi_write_png(filename, img_width, img_height, channels, imgbuffer.data(), 0);
+
     std::cerr << std::endl << "Done" << std::endl;
     return 0;
 }
